60f0d13557b9d10da49666800e42d52c3e7bfc71
PrasannaSh1 <pasashinde@gmail.com>
2026-07-14 01:44:11 +0530

    Final Commit

--- app.py
+++ app.py
@@ -320,4 +320,6 @@ def delete_expense(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    debug = "PORT" not in os.environ
+    app.run(host="0.0.0.0", port=port, debug=debug)
